721beb4c68c1c204a6bbb30b59a497e9818f9fec
ajay <ajay199109@gmail.com>
2021-07-09 11:47:20 +0530

    Completed step3 for course information exercise.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const Header = (props) => {
+  console.log(props.course)
   return (
+      <>
+      {console.log(props.course)}
       <h1>{props.course}</h1>
+      </>
   )
 }
 
@@ -15,9 +19,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} count={props.count1} />
-      <Part part={props.part2} count={props.count2} />
-      <Part part={props.part3} count={props.count3} />
+      <Part part={props.part1.name} count={props.part1.exercises} />
+      <Part part={props.part2.name} count={props.part2.exercises} />
+      <Part part={props.part3.name} count={props.part3.exercises} />
     </div>
   )
 }
@@ -31,18 +35,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} count1={exercises1} part2={part2} count2={exercises2} part3={part3} count3={exercises3} />
-      <Total count1={exercises1} count2={exercises2} count3={exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total count1={part1.exercises} count2={part2.exercises} count3={part3.exercises} />
     </div>
   )
 }
